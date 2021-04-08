d5103fd0bb96df8c75527b81d2f78eb4fab7c506
aman1210 <aman.srivastava101@gmail.com>
2021-04-08 18:36:09 +0530

    updating product routes

--- models/product.js
+++ models/product.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
+  price: { type: Number, required: true },
   description: { type: String, required: true },
   seller: { type: String, required: true },
   productImage: { type: String, required: true },
--- routes/products.js
+++ routes/products.js
@@ -34,40 +34,104 @@ const upload = multer({
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Inside products get request",
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            description: doc.description,
+            seller: doc.seller,
+            productImage: doc.productImage,
+          };
+        }),
+      };
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error });
+    });
 });
 
-router.post("/", (req, res, next) => {
-  res.status(201).json({
-    message: "Inside products post request",
-  });
-});
+router.post(
+  "/",
+  upload.single("productImage"),
+  (req, res, next) => {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      seller: req.body.seller,
+      image: req.file.path,
+    });
+    product
+      .save()
+      .then((result) => {
+        res.status(201).json({
+          message: "Product created!",
+          product: result,
+        });
+      })
+      .catch((error) => {
+        res
+          .status(error.status || 500)
+          .json({ error: error });
+      });
+  }
+);
 
 router.get("/:id", (req, res, next) => {
-  var id = req.params.id;
-  if (id === "aman") {
-    res.status(200).json({
-      message: "You got special",
+  Product.findById(req.params.id)
+    .exec()
+    .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({
+          message: "No valid entry found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error });
     });
-  } else {
-    res.status(200).json({
-      message: "Your product id is " + id,
-    });
-  }
 });
 
 router.patch("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.save;
+  }
+  Product.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      res
+        .status(200)
+        .json({ message: "Product updated successfully!" });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
   res.status(200).json({
     message: "product updated successfully",
   });
 });
 
 router.delete("/:id", (req, res, next) => {
-  res.status(200).json({
-    message: "product deleted successfully",
-  });
+  Product.remove({ _id: req.params.id })
+    .exec()
+    .then((res) => {
+      res.status(200).json({
+        message: "Product deleted successfully",
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;
