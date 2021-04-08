8e881a08642410b3e2374f65fc92931908dc1d53
aman1210 <aman.srivastava101@gmail.com>
2021-04-08 18:04:41 +0530

    setting up multer

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,35 @@
 const express = require("express");
+const multer = require("multer");
+const Product = require("../models/product");
+
+const stroage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().toISOString + file.filename);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: stroage,
+  limits: {
+    fileSize: 1024 * 1024 * 2,
+  },
+  fileFilter: fileFilter,
+});
 
 const router = express.Router();
 
