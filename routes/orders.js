598af5c4b2be76dd27ca4515089972575c65e810
aman1210 <aman.srivastava101@gmail.com>
2021-04-12 11:46:32 +0530

    orders route tweaking

--- routes/orders.js
+++ routes/orders.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const Order = require("../models/orders");
+const Product = require("../models/product");
 
 const router = express.Router();
 
@@ -29,24 +30,63 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const order = new Order({
-    date: req.body.date,
-    products: req.body.products,
-    quantity: req.body.quantity,
-  });
-  order
-    .save()
-    .then((result) => {
-      res.status(201).json({
-        message: "Order created!",
-        result: result,
+  const list = req.body.products; //...an array filled with values
+  var flag = true;
+
+  const functionWithPromise = (item) => {
+    //a function that returns a promise
+    return Product.findOne(
+      { _id: item },
+      "_id",
+      (err, doc) => {
+        if (!doc) {
+          flag = false;
+          return false;
+        }
+        return true;
+      }
+    );
+  };
+
+  const anAsyncFunction = async (item) => {
+    return functionWithPromise(item);
+  };
+
+  const getData = async () => {
+    return Promise.all(
+      list.map((item) => anAsyncFunction(item))
+    );
+  };
+
+  getData().then((data) => {
+    // const index = data.find(null);
+    if (flag) {
+      const order = new Order({
+        date: req.body.date,
+        products: req.body.products,
+        quantity: req.body.quantity,
       });
-    })
-    .catch((error) => {
-      res
-        .status(error.status || 500)
-        .json({ error: error });
-    });
+      order
+        .save()
+        .then((result) => {
+          res.status(201).json({
+            message: "Order created!",
+            result: result,
+          });
+        })
+        .catch((error) => {
+          res
+            .status(error.status || 500)
+            .json({ error: error });
+        });
+    } else {
+      res.status(500).json({
+        message: "Order made for invalid product(s)",
+      });
+    }
+  });
+  // if (products.length > 0) {
+  // }
 });
 
 router.get("/:id", (req, res, next) => {
