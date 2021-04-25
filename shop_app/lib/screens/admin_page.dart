3d7fa00b57f2a5ce9b44fab104b9ac7407dcc308
aman1210 <aman.srivastava101@gmail.com>
2021-04-25 11:32:30 +0530

    working on admin page

--- shop_app/lib/screens/admin_page.dart
+++ shop_app/lib/screens/admin_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/widgets/custom_app_drawer.dart';
 
 class AdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('FlutterStore'),
+      ),
+      drawer: CustomDrawer(12101998),
       body: Center(
         child: Text('Welcome Admin'),
       ),
