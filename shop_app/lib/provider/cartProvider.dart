bb21eacb5137a4eed83bda1dbb227d4f4dcbd2f8
aman1210 <aman.srivastava101@gmail.com>
2021-04-27 15:39:58 +0530

    adding cart badge

--- shop_app/lib/provider/cartProvider.dart
+++ shop_app/lib/provider/cartProvider.dart
@@ -1,6 +1,39 @@
 import 'package:flutter/foundation.dart';
+import 'package:shop_app/models/cart.dart';
 
 class Cart with ChangeNotifier {
   final String _userId;
+  Map<String, CartItem> _items = {};
+  List<String> _ps = [];
+  List<int> _q = [];
+
   Cart(this._userId);
+
+  void addItem(String pId, int price, String title) {
+    if (_items.containsKey(pId)) {
+      _items.update(
+        pId,
+        (value) => CartItem(
+            id: value.id,
+            price: value.price,
+            quantity: value.quantity + 1,
+            title: value.title),
+      );
+      var index = _ps.indexWhere((element) => element == pId);
+      _q[index] += 1;
+    } else {
+      _items.putIfAbsent(
+        pId,
+        () => CartItem(
+          id: pId,
+          price: price,
+          quantity: 1,
+          title: title,
+        ),
+      );
+      _ps.add(pId);
+      _q.add(1);
+    }
+    notifyListeners();
+  }
 }
