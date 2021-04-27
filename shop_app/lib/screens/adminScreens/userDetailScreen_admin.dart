d5b5c91dfd6d773d208811a1a3c8c4b8a363ab37
aman1210 <aman.srivastava101@gmail.com>
2021-04-27 08:31:07 +0530

    fixing userDetailScreen in admin

--- shop_app/lib/screens/adminScreens/userDetailScreen_admin.dart
+++ shop_app/lib/screens/adminScreens/userDetailScreen_admin.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/models/product.dart';
@@ -17,16 +18,88 @@ class UserDetailScreenAdmin extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        child: Column(
-          children: [
-            Text(_user.email),
-            Text(_user.firstname),
-            Text(_user.lastname),
-            ...product.map((e) {
-              return Text(e.name);
-            }).toList()
-          ],
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "User information",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.black, fontSize: 28),
+              ),
+              UserInformation(
+                heading: 'Email',
+                value: _user.email,
+              ),
+              UserInformation(
+                  heading: 'Name',
+                  value: '${_user.firstname} ${_user.lastname}'),
+              UserInformation(heading: 'Phone no.', value: _user.phoneNo ?? ''),
+              UserInformation(heading: 'Address', value: _user.address ?? ''),
+              Text(
+                "Products",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.black, fontSize: 24),
+              ),
+              ...product.map((e) {
+                return Card(
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.all(4),
+                    leading: CachedNetworkImage(
+                      imageUrl: e.image,
+                      height: 60,
+                      width: 60,
+                    ),
+                    title: Text(e.name),
+                  ),
+                );
+              }).toList(),
+              Text(
+                "Orders",
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.black, fontSize: 24),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class UserInformation extends StatelessWidget {
+  const UserInformation({
+    Key? key,
+    required this.heading,
+    required this.value,
+  });
+
+  final String heading;
+  final String value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        children: [
+          Text(
+            '$heading : ',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Text(
+            value,
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+        ],
       ),
     );
   }
