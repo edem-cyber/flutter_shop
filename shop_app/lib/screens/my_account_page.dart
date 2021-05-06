a686569fbf5d6afd621348ec3769391838292704
aman1210 <aman.srivastava101@gmail.com>
2021-05-06 16:23:54 +0530

    updating myAccountPage

--- shop_app/lib/screens/my_account_page.dart
+++ shop_app/lib/screens/my_account_page.dart
@@ -12,6 +12,7 @@ class MyAccountPage extends StatefulWidget {
 
 class _MyAccountPageState extends State<MyAccountPage> {
   var isLoading = false;
+  GlobalKey<FormState> _key = GlobalKey();
 
   @override
   void initState() {
@@ -32,16 +33,119 @@ class _MyAccountPageState extends State<MyAccountPage> {
   @override
   Widget build(BuildContext context) {
     var details = Provider.of<User>(context).currUser;
-    print(details);
     return Scaffold(
-      appBar: AppBar(),
-      body: isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Center(
-              child: Text('hello'),
-            ),
-    );
+        appBar: AppBar(
+          title: Text('My Account'),
+        ),
+        body: isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Container(
+                constraints: BoxConstraints(maxWidth: 960),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                child: Form(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        maxRadius: 75,
+                        minRadius: 50,
+                        child: Icon(
+                          Icons.person_rounded,
+                          size: 60,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        enabled: false,
+                        initialValue: details['email'],
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          ),
+                          prefixIcon: Icon(Icons.email_outlined),
+                        ),
+                        style: TextStyle(color: Colors.grey),
+                        enableInteractiveSelection: false,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        initialValue: details['firstname'],
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            prefixIcon: Icon(Icons.person_outline),
+                            labelText: 'First Name'),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        initialValue: details['lastname'],
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            prefixIcon: Icon(Icons.person_outline),
+                            labelText: 'Last Name'),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        initialValue: details['address'],
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            prefixIcon: Icon(Icons.home_outlined),
+                            labelText: 'Primary Address'),
+                        maxLines: 3,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      TextFormField(
+                        initialValue: details['phone'],
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                            prefixIcon: Icon(Icons.phone),
+                            labelText: 'Mobile no.'),
+                      )
+                    ],
+                  ),
+                  key: _key,
+                ),
+              ),
+        bottomNavigationBar: isLoading
+            ? null
+            : Container(
+                height: 60,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.check),
+                  label: Text('Submit'),
+                ),
+              ));
   }
 }
